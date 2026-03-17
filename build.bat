2c9d92a3eea4f4e1c936fcc0e29285059734a72d
duzhiwei <dzwwgy@163.com>
2026-03-17 11:24:39 +0800

    cfg

--- build.bat
+++ build.bat
@@ -1,4 +1,4 @@
-SET PATH="D:\Program Files\JetBrains\CLion 2021.2.1\bin\cmake\win\bin\";%PATH%
-cmake -B build -A Win32
-cmake --build build --config Release
+SET PATH=D:\msys64\ucrt64\bin;%PATH%
+D:\msys64\ucrt64\bin\cmake.exe -S . -B build\release -G Ninja -D CMAKE_BUILD_TYPE=Release -D CMAKE_CXX_COMPILER=D:/msys64/ucrt64/bin/g++.exe
+D:\msys64\ucrt64\bin\cmake.exe --build build\release
 pause
--- run.bat
+++ run.bat
@@ -1,3 +1,7 @@
-SET PATH=%%~dp0Release%;%PATH
+SET PATH=D:\msys64\ucrt64\bin;%PATH%
+
+SET PATH=%~dp0Release;%PATH%
+SET PATH=%~dp0build\Release\Release;%PATH%
+
 ccbi2ccb K:\PrjMineGit\QCardJS\fruit_hua\480\001_en.ccbi
 pause
